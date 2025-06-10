a0794b129f4699e3e71f8b0c690a05c4b96d0d84
Maddy Foley <maddyfoley5@gmail.com>
2025-06-10 08:24:48 -0400

    parse file edit

--- app/wikipedia/spacy/parse.py
+++ app/wikipedia/spacy/parse.py
@@ -25,46 +25,15 @@ TOKEN:
 
 """
 
-
-
-
-def read_files_by_dir(directory):
+def read_files_by_dir(base_directory):
     res = []
-    for file in os.listdir(directory):
-        fp = os.path.join(directory,file)
-        file_name = file[:-5]
-        file_sem = nlp(file_name)
-        dic = {}
-        # dic[file_name] = set()
-        with open(fp,'r') as html:
-            soup = BeautifulSoup(html, 'html.parser')
-            # if "may refer to" in soup.text.lower():
-            #     print(file_name)
-                
-            if soup.li:
-                text = soup.li.text.strip() + soup.ul.text.strip()
-                
-                doc = nlp(text)
-                # build_word_list= set()
-                build_word_list = []
-                # for chunk in doc.noun_chunks:
-                for token in doc:
-                    if token.pos_ == "NOUN":
-                        print(token.text,[child for child in token.children])
-                    # if file_sem.similarity(token) >= .7:
-                    #     print(token.text)
-                    #     # build_word_list.add(token.text.lower())
-                    # if file_name in token.text:
-                    #     break
-                # dic[file_name] = build_word_list
-    #     dic[file_name] += build_word_list
-    # return res
-
-
-for c in categories:
-    directory = f"app/wikipedia/html/{c}"
-    res = read_files_by_dir(directory)
-    # categories[c] += res
-
-# with open("app/wikipedia/parsed/apparel-accessories-fabric.json",'w') as f:
-#     f.write(json.dump(res))
+    for c in categories:
+        directory = base_directory + c
+        for file in os.listdir(directory):
+            fp = os.path.join(directory,file)
+            file_name = file[:-5]
+            file_sem = nlp(file_name)
+            with open(fp,'r') as html:
+                soup = BeautifulSoup(html, 'html.parser')
+            
+read_files_by_dir("app/wikipedia/html/")
