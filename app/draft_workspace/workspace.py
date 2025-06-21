d72ec6f3a8f5b28cb6a6e5a4e42da4e8668c1c77
Maddy Foley <maddyfoley5@gmail.com>
2025-06-21 16:31:05 -0400

    fixed json file lemma items

--- app/draft_workspace/workspace.py
+++ app/draft_workspace/workspace.py
@@ -5,17 +5,21 @@ nlp = spacy.load('en_core_web_trf')
 
 json_dir = CustomJSONFile('app/data/google_json/google_clean_01')
 my_files = json_dir.get_all_file_paths()
-
+my_lemmas = json_dir.get_content_from_file_path('app/data/google_json/google_clean_01/lemma_items_01.json')
+print(my_lemmas)
 all_items_lemmas = []
 tot = 0
-for item in my_files:
-    content = json_dir.get_content_from_file_path(item)
-    
-all_items_lemmas = []
-# print(tot)
-for c in content:
-    for key in c:
-        all_items_lemmas.append(key)
+# for item in my_files:
+#     content = json_dir.get_content_from_file_path(item)
+#     for obj in content:
+#         for key in obj:
+#             all_items_lemmas.append(key)
+# json_dir.write_new_json_file(all_items_lemmas,'lemma_items')
+# all_items_lemmas = []
+# # print(tot)
+# for c in content:
+#     for key in c:
+#         all_items_lemmas.append(key)
         # print(key,len(c[key]))
         # for obj in content:
         #     print(obj)
@@ -24,7 +28,7 @@ for c in content:
     #             for t in doc:
     #                 print(t.ent_type_)
     #         break
-json_dir.write_new_json_file(all_items_lemmas,'lemma_items')
+
 
 
 # print([c.keys() for c in content])
